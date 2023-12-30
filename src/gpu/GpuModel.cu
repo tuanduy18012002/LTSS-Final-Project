e027d3a9f6e53fe51b849c51193056bc3a2ed0a5
BoyGhost02 <duykhanh100902@gmail.com>
2023-12-30 21:52:35 +0700

    wrong name variable

--- src/gpu/GpuModel.cu
+++ src/gpu/GpuModel.cu
@@ -1,5 +1,7 @@
 #include "GpuModel.h"
 
+#define TILE_WIDTH 16
+
 void GPU_Info::printGpuInfo()
 {
     cudaDeviceProp devProv;
@@ -114,26 +116,26 @@ void GPU_Conv::conv_forward_gpu(float* output, const float* input, const float* 
 
     // Cấp phát bộ nhớ trên thiết bị
     float *device_input, *device_output, *device_weight;
-    cudaMalloc((void **)&device_input, num_samples * input_channel * height_in * width_in * sizeof(float));  // Bản đồ đặc trưng đầu vào có kích thước input_channel
-    cudaMalloc((void **)&device_output, num_samples * output_channel * height_out * width_out * sizeof(float));  // Bản đồ đặc trưng đầu ra có kích thước output_channel
-    cudaMalloc((void **)&device_weight, output_channel * input_channel * height_kernel * height_kernel * sizeof(float));  // Bộ lọc kích thước input_channel * output_channel có kích thước height_kernel * height_kernel
+    cudaMalloc((void **)&device_input, n_sample * input_channel * height_in * width_in * sizeof(float));  // Bản đồ đặc trưng đầu vào có kích thước input_channel
+    cudaMalloc((void **)&device_output, n_sample * channel_out * height_out * width_out * sizeof(float));  // Bản đồ đặc trưng đầu ra có kích thước channel_out
+    cudaMalloc((void **)&device_weight, channel_out * input_channel * height_kernel * height_kernel * sizeof(float));  // Bộ lọc kích thước input_channel * channel_out có kích thước height_kernel * height_kernel
 
     // Sao chép dữ liệu đầu vào và trọng số từ máy chủ đến thiết bị
-    cudaMemcpy(device_input, input_data, num_samples * input_channel * height_in * width_in * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_weight, weight_data, output_channel * input_channel * height_kernel * height_kernel * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_input, input, n_sample * input_channel * height_in * width_in * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_weight, weight, channel_out * input_channel * height_kernel * height_kernel * sizeof(float), cudaMemcpyHostToDevice);
 
     // Đặt kích thước grid và block cho kernel và gọi kernel
     int height_grid = ceil(1.0 * height_out / TILE_WIDTH);
     int width_grid = ceil(1.0 * width_out / TILE_WIDTH);
     int Z = height_grid * width_grid;
     dim3 num_threads_per_block(TILE_WIDTH, TILE_WIDTH, 1);
-    dim3 num_blocks_in_grid(num_samples, output_channel, Z);
+    dim3 num_blocks_in_grid(n_sample, channel_out, Z);
 
     // Gọi kernel
-    conv_forward_kernel<<<num_blocks_in_grid, num_threads_per_block, TILE_WIDTH * TILE_WIDTH * sizeof(float)>>>(device_output, device_input, device_weight, num_samples, output_channel, input_channel, height_in, width_in, height_kernel);
+    conv_forward_kernel<<<num_blocks_in_grid, num_threads_per_block, TILE_WIDTH * TILE_WIDTH * sizeof(float)>>>(device_output, device_input, device_weight, n_sample, channel_out, input_channel, height_in, width_in, height_kernel);
 
     // Sao chép kết quả đầu ra từ thiết bị về máy chủ
-    cudaMemcpy(output_data, device_output, num_samples * output_channel * height_out * width_out * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(output, device_output, n_sample * channel_out * height_out * width_out * sizeof(float), cudaMemcpyDeviceToHost);
 
     // Giải phóng bộ nhớ trên thiết bị
     cudaFree(device_input);
